fece5dbd2a8be65f46e84d163dbaf1a2a234f1b1
manasseh-zw <manassehtchanga@gmail.com>
2025-04-18 19:19:37 +0200

    feat: install deps, setup fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from apollo-webspider!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def hello():
+    return {"message": "hello"}
